53e6ab14faabbfc73e5853e1728e338ae9710ce2
brija <jak5970@rit.edu>
2024-02-25 14:01:07 -0500

    update CheckoutDAO file

--- Homie_Helper/src/main/java/com/need/api/needapi/persistence/CheckoutDAO.java
+++ Homie_Helper/src/main/java/com/need/api/needapi/persistence/CheckoutDAO.java
@@ -6,6 +6,9 @@ import com.need.api.needapi.model.Need;
 //connect to inventory
 //connect to funding basket
 
+//checkout has everything in funding basket
+//get each item in funding basket and subtract quantity
+//clear checkout nad funding basket items
 /**
  * Defines the interface for Checkout persistence
  * The funding basket should have the ability to add and remove items
@@ -21,7 +24,7 @@ public interface CheckoutDAO {
      * 
      * @throws IOException if an issue with underlying storage
      */
-    Basket[] getBasket() throws IOException;
+    Checkout[] getBasket() throws IOException;
 
     /**
      * Retrieves a {@linkplain Fundingbasket needs}
@@ -30,43 +33,21 @@ public interface CheckoutDAO {
      * 
      * @throws IOException if an issue with underlying storage
      */
-    Basket clearBasket() throws IOException;
+    Checkout clearBasket() throws IOException;
 
+    //inventory will either use the need json file or copy it so that
+    //the quantity can be decremented
+    //not sure if we're keeping track of prices and how much bought
     /**
-     * Creates and saves a {@linkplain Need needs}
-     * 
-     * @param need {@linkplain Need needs} object to be created and saved
-     * <br>
-     * The id of the need object is ignored and a new uniqe id is assigned
-     *
-     * @return new {@link Need need} if successful, false otherwise 
-     * 
-     * @throws IOException if an issue with underlying storage
-     */
-    Need createNeed(Need need) throws IOException;
-
-    /**
-     * Updates and saves a {@linkplain Need need}
+     * Updates and saves a {@linkplain Inventory need}
      * 
      * @param {@link Need need} object to be updated and saved
      * 
-     * @return updated {@link Need need} if successful, null if
+     * @return updated {@link Inventory need} if successful, null if
      * {@link Need need} could not be found
      * 
      * @throws IOException if underlying storage cannot be accessed
      */
-    Need updateNeed(Need need) throws IOException;
+    Checkout updateInventory(Inventory need) throws IOException;
 
-    /**
-     * Deletes a {@linkplain Need need} with the given id
-     * 
-     * @param id The id of the {@link Need need}
-     * 
-     * @return true if the {@link Need need} was deleted
-     * <br>
-     * false if need with the given id does not exist
-     * 
-     * @throws IOException if underlying storage cannot be accessed
-     */
-    boolean deleteNeed(int id) throws IOException;
 }
