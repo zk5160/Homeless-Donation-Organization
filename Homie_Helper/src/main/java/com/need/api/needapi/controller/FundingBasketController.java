bc06a445ab9a80897ed47a00b0357fc82d0c7a9c
Eliana <egd1486@rit.edu>
2024-02-26 18:03:15 -0500

    edited the fundingbasket files

---
+++ Homie_Helper/src/main/java/com/need/api/needapi/controller/FundingBasketController.java
@@ -0,0 +1,205 @@
+package com.need.api.needapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.need.api.needapi.persistence.FundingBasketDAO;
+import com.need.api.needapi.model.FundingBasket;
+
+/**
+ * Handles the REST API requests for the FundingBasket resource
+ * <p>
+ * {@literal @}RestController Spring annotation identifies this class as a REST API
+ * method handler to the Spring framework
+ * 
+ * @author SWEN Faculty
+ * @author modifications by Team 7G
+ */
+
+@RestController
+@RequestMapping("fundingbasket")
+public class FundingBasketController {
+    private static final Logger LOG = Logger.getLogger(FundingBasketController.class.getName());
+
+    private FundingBasketDAO fundingbasketDao;
+
+    /**
+     * Creates a REST API controller to reponds to requests
+     * 
+     * @param fundingbasketDao The {@link FundingBasketDAO FundingBasket Data Access Object} to perform CRUD operations
+     * <br>
+     * This dependency is injected by the Spring Framework
+     */
+    public FundingBasketController(FundingBasketDAO fundingbasketDao) {
+        this.fundingbasketDao = fundingbasketDao;
+    }
+
+    /**
+     * Responds to the GET request for a {@linkplain FundingBasket fundingbaskets} for the given id
+     * 
+     * @param id The id used to locate the {@link FundingBasket fundingbasket}
+     * 
+     * @return ResponseEntity with {@link FundingBasket fundingbasket} object and HTTP status of OK if found<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("/{id}")
+    public ResponseEntity<FundingBasket> getFundingBasket(@PathVariable int id) {
+        LOG.info("GET /fundingbasket/" + id);
+        try {
+            FundingBasket fundingbasket = fundingbasketDao.getFundingBasket(id);
+            if (fundingbasket != null)
+                return new ResponseEntity<FundingBasket>(fundingbasket,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Responds to the GET request for all {@linkplain FundingBasket fundingbaskets}
+     * 
+     * @return ResponseEntity with array of {@link FundingBasket fundingbasket} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("")
+    public ResponseEntity<FundingBasket[]> getFundingBasket() {
+        LOG.info("GET /fundingbasket");
+
+        try{
+        FundingBasket[] fundingbasket = fundingbasketDao.getFundingBasket();
+
+        return new ResponseEntity<FundingBasket[]>(fundingbasket,HttpStatus.OK);
+    }
+
+            catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Responds to the GET request for all {@linkplain FundingBasket fundingbaskets} whose name contains
+     * the text in name
+     * 
+     * @param name The name parameter which contains the text used to find the {@link FundingBasket fundingbasket}
+     * 
+     * @return ResponseEntity with array of {@link FundingBasket FundingBasket} objects (may be empty) and
+     * HTTP status of OK<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * <p>
+     * Example: Find all fundingbaskets that contain the text "ma"
+     * GET http://localhost:8080/fundingbasket/?name=ma
+     */
+    @GetMapping("/")
+    public ResponseEntity<FundingBasket[]> searchFundingBasket(@RequestParam String name) {
+        LOG.info("GET /fundingbasket/?name="+name);
+
+        try {
+            FundingBasket[] fundingbasket = fundingbasketDao.findFundingBasket(name);
+            return new ResponseEntity<FundingBasket[]>(fundingbasket,HttpStatus.OK);
+        }
+
+            catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Creates a {@linkplain FundingBasket fundingbaskets} with the provided fundingbasket object
+     * 
+     * @param fundingbasket - The {@link FundingBasket fundingbasket} to create
+     * 
+     * @return ResponseEntity with created {@link FundingBasket fundingbasket} object and HTTP status of CREATED<br>
+     * ResponseEntity with HTTP status of CONFLICT if {@link fundingbasket fundingbasket} object already exists<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PostMapping("")
+    public ResponseEntity<FundingBasket> createFundingBasket(@RequestBody FundingBasket fundingbasket) {
+        LOG.info("POST /fundingbasket " + fundingbasket);
+
+        try {
+            FundingBasket h = fundingbasketDao.createFundingBasket(fundingbasket);
+
+            return new ResponseEntity<FundingBasket>(h,HttpStatus.OK);
+        }
+
+            catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Updates the {@linkplain FundingBasket fundingbaskets} with the provided {@linkplain FundingBasket fundingbaskets} object, if it exists
+     * 
+     * @param fundingbasket The {@link FundingBasket fundingbasket} to update
+     * 
+     * @return ResponseEntity with updated {@link FundingBasket fundingbasket} object and HTTP status of OK if updated<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @PutMapping("")
+    public ResponseEntity<FundingBasket> updateFundingBasket(@RequestBody FundingBasket fundingbasket) {
+        LOG.info("PUT /fundingbasket " + fundingbasket);
+
+        try {
+            FundingBasket h = fundingbasketDao.updateFundingBasket(fundingbasket);
+            if (h != null)
+                return new ResponseEntity<FundingBasket>(h,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+            catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Deletes a {@linkplain FundingBasket fundingbaskets} with the given id
+     * 
+     * @param id The id of the {@link FundingBasket fundingbasket} to deleted
+     * 
+     * @return ResponseEntity HTTP status of OK if deleted<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<FundingBasket>deleteFundingBasket(@PathVariable int id) {
+        LOG.info("DELETE /fundingbasket/" + id);
+
+        try {
+            boolean fundingbasket = fundingbasketDao.deleteFundingBasket(id);
+
+            if (fundingbasket == true)
+                return new ResponseEntity<FundingBasket>(HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+            catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ Homie_Helper/src/main/java/com/need/api/needapi/persistence/FundingBasketFileDAO.java
@@ -0,0 +1,226 @@
+package com.need.api.needapi.persistence;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.need.api.needapi.model.FundingBasket;
+
+/**
+ * Implements the functionality for JSON file-based peristance for FundingBaskets
+ * 
+ * {@literal @}Component Spring annotation instantiates a single instance of this
+ * class and injects the instance into other classes as fundingbasketed
+ * 
+ * @author SWEN Faculty
+ * @author modificaitons by Team 7G
+ */
+@Component
+public class FundingBasketFileDAO implements FundingBasketDAO {
+    private static final Logger LOG = Logger.getLogger(FundingBasketFileDAO.class.getName());
+    Map<Integer, FundingBasket> fundingbaskets;   // Provides a local cache of the fundingbasket objects
+                                // so that we don't fundingbasket to read from the file
+                                // each time
+    private ObjectMapper objectMapper;  // Provides conversion between FundingBasket
+                                        // objects and JSON text format written
+                                        // to the file
+    private static int nextId;  // The next Id to assign to a new fundingbasket
+    private String filename;    // Filename to read from and write to
+
+    /**
+     * Creates a FundingBasket File Data Access Object
+     * 
+     * @param filename Filename to read from and write to
+     * @param objectMapper Provides JSON Object to/from Java Object serialization and deserialization
+     * 
+     * @throws IOException when file cannot be accessed or read from
+     */
+    public FundingBasketFileDAO(@Value("${fundingbaskets.file}") String filename,ObjectMapper objectMapper) throws IOException {
+        this.filename = filename;
+        this.objectMapper = objectMapper;
+        load();  // load the fundingbaskets from the file
+    }
+
+    /**
+     * Generates the next id for a new {@linkplain FundingBasket fundingbasket}
+     * 
+     * @return The next id
+     */
+    private synchronized static int nextId() {
+        int id = nextId;
+        ++nextId;
+        return id;
+    }
+
+    /**
+     * Generates an array of {@linkplain FundingBasket fundingbaskets} from the tree map
+     * 
+     * @return  The array of {@link FundingBasket fundingbaskets}, may be empty
+     */
+    private FundingBasket[] getFundingBasketArray() {
+        return getFundingBasketArray(null);
+    }
+
+    /**
+     * Generates an array of {@linkplain FundingBasket fundingbaskets} from the tree map for any
+     * {@linkplain FundingBasket fundingbaskets} that contains the text specified by containsText
+     * <br>
+     * If containsText is null, the array contains all of the {@linkplain FundingBasket fundingbaskets}
+     * in the tree map
+     * 
+     * @return  The array of {@link FundingBasket fundingbaskets}, may be empty
+     */
+    private FundingBasket[] getFundingBasketArray(String containsText) { // if containsText == null, no filter
+        ArrayList<FundingBasket> fundingbasketArrayList = new ArrayList<>();
+
+        for (FundingBasket fundingbasket : fundingbaskets.values()) {
+            if (containsText == null || fundingbasket.getName().contains(containsText)) {
+                fundingbasketArrayList.add(fundingbasket);
+            }
+        }
+
+        FundingBasket[] fundingbasketArray = new FundingBasket[fundingbasketArrayList.size()];
+        fundingbasketArrayList.toArray(fundingbasketArray);
+        return fundingbasketArray;
+    }
+
+    /**
+     * Saves the {@linkplain FundingBasket fundingbaskets} from the map into the file as an array of JSON objects
+     * 
+     * @return true if the {@link FundingBasket fundingbaskets} were written successfully
+     * 
+     * @throws IOException when file cannot be accessed or written to
+     */
+    private boolean save() throws IOException {
+        FundingBasket[] fundingbasketArray = getFundingBasketArray();
+
+        // Serializes the Java Objects to JSON objects into the file
+        // writeValue will thrown an IOException if there is an issue
+        // with the file or reading from the file
+        objectMapper.writeValue(new File(filename),fundingbasketArray);
+        return true;
+    }
+
+    /**
+     * Loads {@linkplain FundingBasket fundingbaskets} from the JSON file into the map
+     * <br>
+     * Also sets next id to one more than the greatest id found in the file
+     * 
+     * @return true if the file was read successfully
+     * 
+     * @throws IOException when file cannot be accessed or read from
+     */
+    private boolean load() throws IOException {
+        fundingbaskets = new TreeMap<>();
+        nextId = 0;
+
+        // Deserializes the JSON objects from the file into an array of fundingbaskets
+        // readValue will throw an IOException if there's an issue with the file
+        // or reading from the file
+        FundingBasket[] fundingbasketArray = objectMapper.readValue(new File(filename),FundingBasket[].class);
+
+        // Add each fundingbasket to the tree map and keep track of the greatest id
+        for (FundingBasket fundingbasket : fundingbasketArray) {
+            fundingbaskets.put(fundingbasket.getId(), fundingbasket);
+            if (fundingbasket.getId() > nextId)
+                nextId = fundingbasket.getId();
+        }
+        // Make the next id one greater than the maximum from the file
+        ++nextId;
+        return true;
+    }
+
+    /**
+    ** {@inheritDoc}
+     */
+    @Override
+    public FundingBasket[] getFundingBasket() {
+        synchronized(fundingbaskets) {
+            return getFundingBasketArray();
+        }
+    }
+
+    /**
+    ** {@inheritDoc}
+     */
+    @Override
+    public FundingBasket[] findFundingBasket(String containsText) {
+        synchronized(fundingbaskets) {
+            return getFundingBasketArray(containsText);
+        }
+    }
+
+    /**
+    ** {@inheritDoc}
+     */
+    @Override
+    public FundingBasket getFundingBasket(int id) {
+        synchronized(fundingbaskets) {
+            if (fundingbaskets.containsKey(id))
+                return fundingbaskets.get(id);
+            else
+                return null;
+        }
+    }
+
+    /**
+    ** {@inheritDoc}
+     */
+    @Override
+    public FundingBasket createFundingBasket(FundingBasket fundingbasket) throws IOException {
+        synchronized(fundingbaskets) {
+            // We create a new fundingbasket object because the id field is immutable
+            // and we fundingbasket to assign the next unique id
+            FundingBasket newFundingBasket = new FundingBasket(nextId(), fundingbasket.getName(), fundingbasket.getCost(), fundingbasket.getQuantity(), fundingbasket.getType());
+            fundingbaskets.put(newFundingBasket.getId(), newFundingBasket);
+            save(); // may throw an IOException
+            return newFundingBasket;
+        }
+    }
+
+    /**
+    ** {@inheritDoc}
+     */
+    @Override
+    public FundingBasket updateFundingBasket(FundingBasket fundingbasket) throws IOException {
+        synchronized(fundingbaskets) {
+            if (fundingbaskets.containsKey(fundingbasket.getId()) == false)
+                return null;  // fundingbasket does not exist
+
+            fundingbaskets.put(fundingbasket.getId(),fundingbasket);
+            // fundingbaskets.put(fundingbasket.getCost(), fundingbasket);
+            // fundingbaskets.put(fundingbasket.getQuantity(), fundingbasket);
+            // fundingbaskets.put(Interger.parseInt(fundingbasket.getType),fundingbasket);
+            // fundingbaskets.put(fundingbasket.getId(),fundingbasket.getCost());
+            // fundingbaskets.put(fundingbasket.getId(), fundingbasket.getType());
+            
+            //fundingbaskets.put(fundingbasket.getCost(),fundingbasket);
+            //fundingbaskets.put(fundingbasket.getType(),fundingbasket);
+            save(); // may throw an IOException
+            return fundingbasket;
+        }
+    }
+
+    /**
+    ** {@inheritDoc}
+     */
+    @Override
+    public boolean deleteFundingBasket(int id) throws IOException {
+        synchronized(fundingbaskets) {
+            if (fundingbaskets.containsKey(id)) {
+                fundingbaskets.remove(id);
+                return save();
+            }
+            else
+                return false;
+        }
+    }
+}
