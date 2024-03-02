ce55cb2a77a087ca07dc0f69532ecb8f5f0caac1
brija <jak5970@rit.edu>
2024-03-02 14:27:48 -0500

    remove checkout

--- Homie_Helper/src/test/java/com/need/api/needapi/model/NeedTest.java
+++ Homie_Helper/src/test/java/com/need/api/needapi/model/NeedTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import com.need.api.needapi.model.Need;
-
 //import com.need.api.needapi.model.Need;
 
 /**
