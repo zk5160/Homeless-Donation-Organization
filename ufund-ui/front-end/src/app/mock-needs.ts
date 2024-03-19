fe33ec81448ff6e24818f20405c71a121ae963f8
Eliana <egd1486@rit.edu>
2024-03-18 20:13:30 -0400

    edited mock-needs

---
+++ ufund-ui/front-end/src/app/mock-needs.ts
@@ -0,0 +1,9 @@
+import { Need } from './need';
+
+export const NEEDS: Need[] = [
+  {"id":4,"name":"tent","cost":15.0,"quantity":4,"type":"supplies"},
+{"id":5,"name":"sweater","cost":5.0,"quantity":3,"type":"clothes"},
+{"id":6,"name":"pants","cost":15.0,"quantity":5,"type":"clothes"},
+{"id":7,"name":"socks","cost":2.0,"quantity":1,"type":"clothes"},
+{"id":8,"name":"soup","cost":2.99,"quantity":2,"type":"food"}
+];
