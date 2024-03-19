0547593069b3ed22ce82b455da6ac059c10d1bd5
Eliana <egd1486@rit.edu>
2024-03-18 21:03:30 -0400

    edited file

--- ufund-ui/front-end/src/app/need-detail/need-detail.component.spec.ts
+++ ufund-ui/front-end/src/app/need-detail/need-detail.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeroDetailComponent } from './need-detail.component';
+import { NeedDetailComponent } from './need-detail.component';
 
-describe('HeroDetailComponent', () => {
-  let component: HeroDetailComponent;
-  let fixture: ComponentFixture<HeroDetailComponent>;
+describe('NeedDetailComponent', () => {
+  let component: NeedDetailComponent;
+  let fixture: ComponentFixture<NeedDetailComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroDetailComponent]
+      imports: [NeedDetailComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(HeroDetailComponent);
+    fixture = TestBed.createComponent(NeedDetailComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
