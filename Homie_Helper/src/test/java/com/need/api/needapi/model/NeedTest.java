5d52589bac3c32dc8a92a529f71221889f4612b2
brija <jak5970@rit.edu>
2024-03-19 18:31:00 -0400

    fixed need test

--- Homie_Helper/src/test/java/com/need/api/needapi/model/NeedTest.java
+++ Homie_Helper/src/test/java/com/need/api/needapi/model/NeedTest.java
@@ -58,6 +58,66 @@ public class NeedTest {
     }
 
     @Test
+    public void testQuantity() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        float cost = 10.99f;
+        int quantity = 5;
+        String type = "candy";
+
+        Need need = new Need(id, name, cost, quantity, type);
+
+        int expected_quantity = 6;
+
+        // Invoke
+        need.setQuantity(expected_quantity);
+
+        // Analyze
+        assertEquals(expected_quantity,need.getQuantity());
+    }
+
+    @Test
+    public void testType() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        float cost = 10.99f;
+        int quantity = 5;
+        String type = "candy";
+
+        Need need = new Need(id, name, cost, quantity, type);
+
+        String expected_type = "food";
+
+        // Invoke
+        need.setType(expected_type);
+
+        // Analyze
+        assertEquals(expected_type,need.getType());
+    }
+
+    @Test
+    public void testCost() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        float cost = 10.99f;
+        int quantity = 5;
+        String type = "candy";
+
+        Need need = new Need(id, name, cost, quantity, type);
+
+        float expected_cost = 5.99f;
+
+        // Invoke
+        need.setCost(expected_cost);
+
+        // Analyze
+        assertEquals(expected_cost,need.getCost());
+    }
+
+    @Test
     public void testToString() {
         // Setup
         int id = 99;
