99456bdcba31e6d3af75f13efabd65ff3edc712a
brija <jak5970@rit.edu>
2024-04-07 16:14:03 -0400

    fundingbasket test

--- Homie_Helper/src/test/java/com/need/api/needapi/model/FundingBasketTest.java
+++ Homie_Helper/src/test/java/com/need/api/needapi/model/FundingBasketTest.java
@@ -58,6 +58,66 @@ public class FundingBasketTest {
     }
 
     @Test
+    public void testQuantity() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        float cost = 10.99f;
+        int quantity = 5;
+        String type = "candy";
+
+        FundingBasket basket = new FundingBasket(id, name, cost, quantity, type);
+
+        int expected_quantity = 6;
+
+        // Invoke
+        basket.setQuantity(expected_quantity);
+
+        // Analyze
+        assertEquals(expected_quantity,basket.getQuantity());
+    }
+
+    @Test
+    public void testType() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        float cost = 10.99f;
+        int quantity = 5;
+        String type = "candy";
+
+        FundingBasket basket = new FundingBasket(id, name, cost, quantity, type);
+
+        String expected_type = "food";
+
+        // Invoke
+        basket.setType(expected_type);
+
+        // Analyze
+        assertEquals(expected_type,basket.getType());
+    }
+
+    @Test
+    public void testCost() {
+        // Setup
+        int id = 99;
+        String name = "Wi-Fire";
+        float cost = 10.99f;
+        int quantity = 5;
+        String type = "candy";
+
+        FundingBasket basket = new FundingBasket(id, name, cost, quantity, type);
+
+        float expected_cost = 5.99f;
+
+        // Invoke
+        basket.setCost(expected_cost);
+
+        // Analyze
+        assertEquals(expected_cost,basket.getCost());
+    }
+
+    @Test
     public void testToString() {
         // Setup
         int id = 99;
